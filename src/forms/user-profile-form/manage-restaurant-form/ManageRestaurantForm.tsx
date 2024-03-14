f0d51667c070c37e995fc54257bd824c901901c4
jaayeshe <jubairaayeshe4@gmail.com>
2024-03-14 19:53:00 -0230

    Implemented Menu section

--- src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -5,6 +5,7 @@ import { z } from "zod";
 import DetailsSection from "./DetailsSection";
 import { Separator } from "@radix-ui/react-separator";
 import CuisinesSection from "./CuisinesSection";
+import MenuSection from "./MenuSection";
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -68,6 +69,8 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         <DetailsSection />
         <Separator />
         <CuisinesSection />
+        <Separator />
+        <MenuSection />
       </form>
     </Form>
   );
